bfad910d51f54ce1d90bab56b29a632618126afd
Jim Termini <Jim.Termini@me.com>
2022-04-04 01:19:15 -0400

    Initial Submission

---
+++ Lab1_5.cs
@@ -0,0 +1,20 @@
+﻿class FunctionalMethods
+{
+    public static Func<int, int, int, int> LoneSum()
+    {
+		Func<int, int, int, int> toReturn = (a, b, c) =>
+        {
+          if (a == b && b == c) return 0;
+          if (a == b) return c;
+          if (a == c) return b;
+          if (b == c) return a;
+          return a + b + c;
+        };
+		return toReturn;
+    }
+
+    public static Predicate<int> IsPositive() 
+    {
+        return (x) => x > 0;
+    }
+}
